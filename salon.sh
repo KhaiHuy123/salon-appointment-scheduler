4699f23fed22c7526fbfa2a5acb065446ddccc2f
nguyenkhaihuy <86825653+KhaiHuy123@users.noreply.github.com>
2024-10-09 16:24:12 +0700

    salon.sh

--- salon.sh
+++ salon.sh
@@ -63,7 +63,7 @@ while true; do
   if [ $RETVAL -eq 0 ]; then
     break
   else
-    printf "\nI could not find the service you want. Would you like to choose again ?\n"
+    printf "\nI could not find that service. Would you like to choose again ?\n"
   fi
   ((NO_CUSTOMERS_APPOINTMENT++))
 done
