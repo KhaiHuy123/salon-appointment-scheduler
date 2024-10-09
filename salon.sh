549247dcab4c0491cf2355c0f2a0afcae4bd33de
nguyenkhaihuy <86825653+KhaiHuy123@users.noreply.github.com>
2024-10-09 08:41:41 +0700

    salon.sh

--- salon.sh
+++ salon.sh
@@ -11,12 +11,13 @@ say_hello() {
 # introduce services to customer(s)
 show_services() {
   AVAILABLE_SERVICES=$($PSQL "SELECT * FROM services LIMIT 10;")
-  echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR NAME; do
+  echo "$AVAILABLE_SERVICES" | 
+  while read SERVICE_ID BAR NAME; do
     echo "$SERVICE_ID) $NAME"
   done
 }
 
-# main function
+# serving stage
 serving(){
   local NO_CUSTOMERS_APPOINTMENT=$1
   show_services
@@ -27,17 +28,17 @@ serving(){
   # check if service exist
   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
   if [[ -z $SERVICE_NAME ]]; then
-    echo -e "\nThe service you offer isn't served in our salon"
+    echo -e "\nThe service you offered is not served in our salon"
     return 1
   fi
   
-  printf "\nWhat's your phone number?\n"
+  printf "\nWhat is your phone number, please?\n"
   read CUSTOMER_PHONE
 
-  # ask for customer's name if phone number can't be found
+  # ask for customer's name if phone number not found
   CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
   if [[ -z $CUSTOMER_NAME ]]; then
-    printf "Your phone number doesn't exist in our system, what's your name?\n"
+    printf "Your phone number does not exist in our system, what is your name?\n"
     read CUSTOMER_NAME
     INSERT_NEW_CUSTOMER=$($PSQL "INSERT INTO customers(customer_id, phone, name) VALUES($NO_CUSTOMERS_APPOINTMENT, '$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
   fi
@@ -46,7 +47,7 @@ serving(){
   printf "What time would you like for $SERVICE_NAME, $CUSTOMER_NAME?\n"
   read SERVICE_TIME
 
-  # get customer_id
+  # arrange appointment
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
   INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, appointment_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, $NO_CUSTOMERS_APPOINTMENT, '$SERVICE_TIME')")
   printf "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME.\n"
@@ -62,7 +63,7 @@ while true; do
   if [ $RETVAL -eq 0 ]; then
     break
   else
-    printf "\nI could not find that service. What would you like today?\n"
+    printf "\nI could not find the service you want. Would you like to choose again ?\n"
   fi
   ((NO_CUSTOMERS_APPOINTMENT++))
 done
